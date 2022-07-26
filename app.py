646fa099a41df37c43d97bdfffc2aa32b487db07
Nick Fulghum <nickfulghum92@gmail.com>
2022-07-26 18:02:53 -0500

    Passed first round of test

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Flask app for Cupcakes"""
 from flask import Flask, request, jsonify, render_template
 
-from models import db, connect_db
+from models import db, connect_db, Cupcake
 
 app = Flask(__name__)
 
@@ -10,3 +10,44 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = "mishacat123"
 
 connect_db(app)
+
+
+@app.route('/api/cupcakes')
+def get_cupcakes():
+    all_cupcakes = [cupcake.serialize() for cupcake in Cupcake.query.all()]
+    return jsonify(cupcakes=all_cupcakes)
+
+
+@app.route('/api/cupcakes/<int:id>')
+def get_cupcake(id):
+    cupcake = Cupcake.query.get_or_404(id)
+    return jsonify(cupcake=cupcake.serialize())
+
+
+@app.route('/api/cupcakes', methods=["POST"])
+def create_cupcake():
+    cupcake = Cupcake(flavor=request.json['flavor'],
+                      size=request.json['size'],
+                      rating=request.json['rating'],
+                      image=request.json['image'])
+    db.session.add(cupcake)
+    db.session.commit()
+    response_json = jsonify(cupcake=cupcake.serialize())
+    return (response_json, 201)
+
+
+@app.route('/api/cupcakes/<int:id>')
+def update_cupcake(id):
+    cupcake = Cupcake.query.get_or_404(id)
+
+    cupcake.flavor = request.json.get('flavor', cupcake.flavor)
+    cupcake.size = request.json.get('size', cupcake.size)
+    cupcake.rating = request.json.get('rating', cupcake.rating)
+    cupcake.image = request.json.get('image', cupcake.image)
+
+    db.session.commit()
+    return jsonify(cupcake=cupcake.serialize())
+
+
+@app.route('/api/cupcakes/<int:id>', methods=["DELETE"])
+def delete_cupcake(id):
--- models.py
+++ models.py
@@ -19,3 +19,15 @@ class Cupcake(db.Model):
     size = db.Column(db.Text, nullable=False)
     rating = db.Column(db.Float, nullable=False)
     image = db.Column(db.Text, default='https://tinyurl.com/demo-cupcake')
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'flavor': self.flavor,
+            'size': self.size,
+            'rating': self.rating,
+            'image': self.image
+        }
+
+    def __repr__(self):
+        return f"<Cupcake {self.id} flavor={self.flavor} size={self.size} rating={self.rating} image={self.image} >"
